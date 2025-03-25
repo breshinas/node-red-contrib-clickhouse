5b20d8f39f24c20ea36b617472ba905c5bc88ca6
breshinas <9035788472@mail.ru>
2025-03-25 16:49:42 +0100

    v1.0.0

---
+++ README.md
@@ -0,0 +1,126 @@
+# node-red-contrib-mongodb4
+
+A Clickhouse driver node for Node-Red.
+
+This package includes two nodes for node-red:
+
+**The Config Node**
+
+Connect to your local Clickhouse Server or a Cloud versions.
+
+**The Flow Node**
+
+Execute an query/insert/command within your flow.
+
+_This node was inspired by other projects like [node-red-contrib-mongodb4](https://github.com/steineey/node-red-contrib-mongodb4)
+
+## Installation
+
+Navigate to your .node-red directory - typically `~/.node-red`.
+
+```sh
+npm install --save --omit=dev node-red-contrib-clickhouse
+```
+
+## Compatibility
+
+The latest version of node-red-contrib-clickhouse@1.0.x is tested with the following Clickhouse server versions: 25.3.1
+
+Node-RED >= v3.0.0  
+NodeJS >= v16.20.1
+
+
+## Usage Example
+
+I will add it later ))
+
+## The Configuration Node
+
+### Simple Connection URI
+
+-   **Protocol** - `http` or `https`
+
+-   **Hostname** - Hostname / IP to connect to Clickhouse
+
+-   **Port** - Optional port number. In most cases `8123`.
+
+-   **Pathname** - Custom path name to add to URI. [Details](https://clickhouse.com/docs/integrations/javascript#proxy-with-a-pathname)
+
+### Advanced Connection URI
+
+-   **URI** - Define your own connection string in URI format.
+
+### Authentication (optional)
+
+-   **Username** - Username for authentication.
+
+-   **Password** - Password for authentication.
+
+### Application
+
+-   **Database** - A Clickhouse database name is required.
+
+-   **Application Name** - The name of the application that created this MongoClient instance. MongoDB 3.4 and newer will print this value in the server log upon establishing each connection. It is also recorded in the slow query log and profile collections.
+
+If this field is unspecified, the client node will create a app name for you.
+That looks like this: `nodered-azmr5z97`. The prefix `nodered` is static. `azmr5z97` is a random connection pool id, created on runtime start-up, config-node update and full deployment.
+
+The current app name of a config node is logged to the node-red runtime log.
+
+
+### Connect Options
+
+-   **MaxPoolsize** - Specifies the maximum number of connections the driver should create in its connection pool. This count includes connections in use.
+
+-   **MaxIdleTimeMS** - Specifies the amount of time, in milliseconds, a connection can be idle before it's closed. Specifying 0 means no minimum.
+
+### Connection Pools
+
+Each configuration node has his own connection pool with a default max poolsize of 100 connection at a given time. 
+
+## The Flow Node
+
+Execute MongoDB collection operations with this node.
+
+### Inputs / Options
+
+-   **Connection (clickhouse-client)** - Select a Clickhouse server connection.
+
+-   **Mode | msg.mode (string)** - Decide which operation do you want to use {'query', 'command', 'insert'}
+
+-   **Table | msg.table (string)** - table name for insert operation.
+
+-   **Output | msg.output (string)** - Define output processing for 'query' mode {'toArray', 'forEach'}.  
+'forEach' produce output for each received record in stream manner and additional msg with an payload equal to null.
+
+-   **Format | msg.format (string)** - format output. By default assigned 'JSONEachRow' value but you can define your own if needed.
+
+-   **msg.payload (array | JSON )** - Pass message payload as array for 'insert' mode. For 'query' mode it should be JSON object.
+
+-   **Wait for completion** - With this feature enabled, the operation node will wait until execution were complete. See [wait_end_of_query](https://clickhouse.com/docs/operations/settings/settings#http_wait_end_of_query)
+
+-   **Do not throw error (place it into msg)** - With this feature enabled, the operation node will return error in msg insted of throwing it
+
+### Payload Output
+
+The node will output the database driver response as message payload.
+The mode `query` can output with `toArray` or `forEach`.
+
+
+### Node Status 
+
+Node status information is displayed below the node:
+
+#### Tags
+- **s** : Number of successful executions
+- **err** : Number of failed executions 
+- **rt** : Last execution runtime in ms 
+
+#### Colors
+- **green** : Last execution was successful 
+- **blue** : Node execution in progress 
+- **red** : Last execution failed
+
+### More general driver information
+
+[Visit the Clickhouse Driver Docs](https://clickhouse.com/docs/integrations/javascript)
---
+++ src/clickhouse.js
@@ -0,0 +1,339 @@
+module.exports = function (RED) {
+  const { createClient } = require('@clickhouse/client');
+
+  function randStr() {
+    return Math.floor(Math.random() * Date.now()).toString(36);
+  }
+
+  function isJsonObject(obj) {
+    return obj !== null && typeof obj === 'object' && !Array.isArray(obj);
+  }
+
+  function isString(value) {
+    return typeof value === 'string' && value.length > 0;
+  }
+
+  function ClientNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+
+    // prepare clickhouse connection uri
+    let clickhouseUri = '';
+    switch (config.uriTabActive) {
+      case 'tab-uri-simple':
+        if (config.protocol && config.hostname) {
+          clickhouseUri = `${config.protocol}://${config.hostname}`;
+        } else {
+          throw new Error('Clickhouse protocol or hostname undefined.');
+        }
+        if (config.port) {
+          clickhouseUri += `:${config.port}`;
+        }
+        break;
+      case 'tab-uri-advanced':
+        if (config.uri) {
+          clickhouseUri = config.uri;
+        } else {
+          throw new Error('Clickhouse URI undefined.');
+        }
+        break;
+      default:
+        throw new Error('Clickhouse uri config failed.');
+    }
+
+    // app name will be printed in db server log upon establishing each connection
+    const appName = config.appName || `nodered-${randStr()}`;
+
+    // init clickhouse client instance
+    node.clickhouseClient = createClient({
+      url: clickhouseUri, // e.g., 'http://localhost:8123'
+      pathname: config.pathname || '',
+      username: node.credentials.username || '',
+      password: node.credentials.password || '',
+      database: config.dbName,
+      application: appName,
+      max_open_connections: parseInt(config.maxPoolSize || '100', 10),
+      // Assuming some queries may exceed 5 minutes execution time
+      request_timeout: 400_000,
+      keep_alive: {
+        enabled: true,
+        idle_socket_ttl: parseInt(config.maxIdleTimeMS || '2500', 10), // idle socket time in ms
+      },
+      // from https://clickhouse.com/docs/integrations/javascript#keep-alive-troubleshooting
+      clickhouse_settings: {
+        send_progress_in_http_headers: 1,
+        http_headers_progress_interval_ms: '110000', // UInt64, passed as a string
+      },
+    });
+
+    // on flow redeployment or node-red instance shutdown
+    node.on('close', async (removed, done) => {
+      done = done || function () { };
+      if (node.clickhouseClient) {
+        // close clickhouse client and all open connections
+        await node.clickhouseClient.close();
+        node.log('client closed');
+      }
+      done();
+    });
+
+    node.log(`client initialized with app name '${appName}'`);
+    //node.log(`node.clickhouseClient '${JSON.stringify(node.clickhouseClient)}'`);
+  }
+
+  RED.nodes.registerType('clickhouse-client', ClientNode, {
+    credentials: {
+      username: { type: 'text' },
+      password: { type: 'password' },
+    },
+  });
+
+  function OperationNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+
+    node.client = RED.nodes.getNode(config.clientNode);
+
+    node.config = {
+      mode: config.mode, // "query" || "insert"
+      table: config.table,
+      query: config.query,
+      output: config.output,
+      //format: config.format || 'JSONEachRow',
+      wait: config.wait,
+      // maxTimeMS: parseInt(config.maxTimeMS || "0", 10),
+	  returnError: config.returnError,
+    };
+
+    // display node metric in node status
+    const metric = {
+      execCtr: 0,
+      successCtr: 0,
+      errorCtr: 0,
+      runtime: 0,
+    };
+
+    // Do not update status faster than x ms
+    const updateStatusPeriodMs = 1000;
+    let statusTimer = null;
+
+    function updateStatus({ fill }) {
+      if (!statusTimer) {
+        statusTimer = setTimeout(() => {
+          node.status({
+            fill: fill,
+            shape: 'dot',
+            text: `s=${metric.successCtr}, err=${metric.errorCtr}, rt=${metric.runtime}ms`,
+          });
+          statusTimer = null;
+        }, updateStatusPeriodMs);
+      }
+    }
+
+    // init
+    updateStatus({ fill: 'green' });
+
+    function execStart() {
+      const execStartTs = Date.now();
+      metric.execCtr++;
+      updateStatus({ fill: 'blue' });
+      return execStartTs;
+    }
+
+    function execSuccess(execStartTs) {
+      metric.execCtr--;
+      metric.successCtr++;
+      metric.runtime = Date.now() - execStartTs;
+      updateStatus({
+        fill: metric.execCtr > 0 ? 'blue' : 'green',
+      });
+    }
+
+    function execError(execStartTs) {
+      metric.execCtr--;
+      metric.errorCtr++;
+      metric.runtime = Date.now() - execStartTs;
+      updateStatus({
+        fill: 'red',
+      });
+    }
+
+    node.on('input', async (msg, send, done) => {
+      done = done || function () { };
+
+      const execStartTs = execStart();
+
+      try {
+        // ============================================================
+
+        /* // ==== we can stream result
+        const rows = await client.query({
+          query: 'SELECT number FROM system.numbers_mt LIMIT 5',
+          format: 'JSONEachRow', // or JSONCompactEachRow, JSONStringsEachRow, etc.
+        })
+        const stream = rows.stream()
+        stream.on('data', (rows: Row[]) => {
+          rows.forEach((row: Row) => {
+          console.log(row.json()) // or `row.text` to avoid parsing JSON
+          })
+        })
+        await new Promise((resolve, reject) => {
+          stream.on('end', () => {
+          console.log('Completed!')
+          resolve(0)
+          })
+          stream.on('error', reject)
+        })
+        */
+
+        /* // ==== we can output result as CSV
+        const resultSet = await client.query({
+          query: 'SELECT number FROM system.numbers_mt LIMIT 5',
+          format: 'CSV', // or TabSeparated, CustomSeparated, etc.
+        })
+        const stream = resultSet.stream()
+        stream.on('data', (rows: Row[]) => {
+          rows.forEach((row: Row) => {
+          console.log(row.text)
+          })
+        })
+        await new Promise((resolve, reject) => {
+          stream.on('end', () => {
+          console.log('Completed!')
+          resolve(0)
+          })
+          stream.on('error', reject)
+        })
+        */
+
+        /* // ==== we can stream results
+        const resultSet = await client.query({
+          query: 'SELECT number FROM system.numbers LIMIT 10',
+          format: 'JSONEachRow', // or JSONCompactEachRow, JSONStringsEachRow, etc.
+        })
+        for await (const rows of resultSet.stream()) {
+          rows.forEach(row => {
+          console.log(row.json())
+          })
+        }
+        */
+
+        // ============================================================
+
+        // Check for valid client
+        if (!node.client) {
+          throw new Error('Clickhouse config error.');
+        }
+
+        let result;
+        let clickhouse_settings = {};
+        if (config.wait) clickhouse_settings.wait_end_of_query = 1;
+		const mode = msg.mode || node.config.mode;
+
+        switch (mode) {
+          case 'insert':
+            {
+              const tableName = node.config.table || msg.table;
+              if (!isString(tableName)) throw new Error('table name undefined');
+
+              // Prepare operation arguments
+              let values = [];
+              if (msg.payload) {
+                if (Array.isArray(msg.payload)) {
+                  values = msg.payload;
+                } else {
+                  values = [msg.payload];
+                }
+              }
+              if (values.length === 0)
+                throw new Error(
+                  'msg.payload does not contain any item for insert mode. table: ' +
+                  tableName
+                );
+
+              let cmd = {
+                table: tableName,
+                format: 'JSONEachRow',
+                values: values,
+                clickhouse_settings: clickhouse_settings,
+              };
+              result = await node.client.clickhouseClient.insert(cmd);
+
+              msg.payload = result.summary;
+              send(msg);
+            }
+            break;
+
+          case 'command':
+          case 'query':
+          default: {
+            const query = node.config.query || msg.query;
+            if (!isString(query)) throw new Error('query undefined');
+
+            // Use msg.params or, if msg.payload is a JSON object, use it as parameters
+            let params =
+              msg.params || (isJsonObject(msg.payload) ? msg.payload : {});
+
+            let cmd = {
+              query: query,
+              query_params: params,
+              //format: 'JSONEachRow',
+              clickhouse_settings: clickhouse_settings,
+            };
+
+            if (mode == 'command') {
+              result = await node.client.clickhouseClient.command(cmd);
+              send({ ...msg, payload: result });
+            } else {
+			  cmd.format = msg.format || 'JSONEachRow';
+              result = await node.client.clickhouseClient.query(cmd);
+
+              // Handle output processing based on configuration
+              switch (node.config.output) {
+                case 'forEach': {
+                  const stream = result.stream();
+                  await new Promise((resolve, reject) => {
+                    stream.once('end', resolve);
+                    stream.once('error', reject);
+                    stream.on('data', (rows) => {
+                      rows.forEach((row) => {
+                        let json = row.json();
+                        //node.log(`row json= '${json}'`);
+                        send({ ...msg, payload: json });
+                      });
+                    });
+                  });
+                  // send an empty array to indicate completion
+                  send({ ...msg, payload: null });
+                  break;
+                }
+                case 'toArray':
+                default:
+                  send({ ...msg, payload: await result.json() });
+              }
+            }
+          }
+        }
+
+        execSuccess(execStartTs);
+        done();
+      } catch (err) {
+        execError(execStartTs);
+		if (config.returnError) {
+			send({ ...msg, error: err });
+			done();
+		} else {
+			done(err);
+		}
+      }
+    });
+
+    node.on('close', async (removed, done) => {
+      done = done || function () { };
+      // do some removal job ...
+      done();
+    });
+  }
+
+  RED.nodes.registerType('clickhouse', OperationNode);
+};
